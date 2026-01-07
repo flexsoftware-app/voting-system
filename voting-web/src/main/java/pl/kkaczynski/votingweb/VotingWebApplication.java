70c83fe2f0939732dcbebd0193396594c2d89c70
kkaczynski <253564697+flexsoftware-app@users.noreply.github.com>
2026-01-07 19:35:52 +0100

    Initial multi-module voting-system project

---
+++ voting-web/src/main/java/pl/kkaczynski/votingweb/VotingWebApplication.java
@@ -0,0 +1,13 @@
+package pl.kkaczynski.votingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingWebApplication.class, args);
+    }
+
+}
---
+++ voting-web/src/test/java/pl/kkaczynski/votingweb/VotingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kkaczynski.votingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
